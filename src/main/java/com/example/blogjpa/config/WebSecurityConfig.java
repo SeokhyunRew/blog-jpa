b2a2c7e13a5c8252ac866e9fadba7f94d01c4ef9
seokhyunrew <darkknightttt@nave.com>
2024-03-20 10:19:46 +0900

    feat: login & signup upload

---
+++ src/main/java/com/example/blogjpa/config/WebSecurityConfig.java
@@ -0,0 +1,110 @@
+package com.example.blogjpa.config;
+
+//-------------------------------------------------기존 방식 ---------------------------------------
+
+/*import com.example.blogjpa.service.UserDetailService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;*/
+
+/*
+
+@Configuration
+public class WebSecurityConfig {
+    private UserDetailService userDetailService;
+
+    public WebSecurityConfig(UserDetailService userDetailService) {
+        this.userDetailService = userDetailService;
+    }
+
+    @Bean
+    public WebSecurityCustomizer configure() {      // 1) 스프링 시큐리티 기능 비활성화
+        return web -> web.ignoring().requestMatchers(toH2Console())
+                .requestMatchers("/static/**");
+    }
+
+    // 2) 특정 HTTP 요청에 대한 웹 기반 보안 구성
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.authorizeHttpRequests()    // 3) 인증, 인가 설정
+                .requestMatchers("/login", "/signup", "/user").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()        //4) 폼 기반 로그인 설정
+                .loginPage("/login")
+                .defaultSuccessUrl("/articles")
+                .and()
+                .logout()       // 5) 로그아웃 설정
+                .logoutSuccessUrl("/login")
+                .invalidateHttpSession(true)
+                .and()
+                .csrf().disable()       // 6) csrf 비활성화
+                .build();
+    }
+
+    // 7) 인증관리자 관련 설정 => 삭제 가능!
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity httpSecurity, BCryptPasswordEncoder bCryptPasswordEncoder, UserDetailService userDetailService) {
+        return httpSecurity.getSharedObject(AuthenticationManagerBuilder.class)
+                .userDetailsService(userDetailService)  // 8) 사용자 정보 서비스 설정
+                .passwordEncoder(bCryptPasswordEncoder)
+                .and()
+                .build();
+    }
+
+    // 9) 패스워드 인코더로 사용할 빈 등록
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}*/
+
+//-------------------------------------Spring Security 6.x 버전부터 변경 사항  >> 기존에 arguments(인수)로 입력받던 방식을 FunctionalInterface로 입력받도록 변경--------------
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
+
+
+@EnableWebSecurity
+@Configuration
+public class WebSecurityConfig {
+
+    @Bean
+    public WebSecurityCustomizer configure() {      // 스프링 시큐리티 기능 비활성화
+        return web -> web.ignoring().requestMatchers(toH2Console())
+                .requestMatchers("/static/**");
+    }
+
+    // 특정 HTTP 요청에 대한 웹 기반 보안 구성
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests(auth ->              // 인증, 인가 설정
+                        auth.requestMatchers("/login", "/signup", "/user").permitAll()
+                                .anyRequest().authenticated())
+                .formLogin(auth -> auth.loginPage("/login")     // 폼 기반 로그인 설정
+                        .defaultSuccessUrl("/articles"))
+                .logout(auth -> auth.logoutSuccessUrl("/login") // 로그아웃 설정
+                        .invalidateHttpSession(true))
+                .csrf(auth -> auth.disable());                  // csrf 비활성화
+        return httpSecurity.build();
+    }
+
+    // 패스워드 인코더로 사용할 빈 등록
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
