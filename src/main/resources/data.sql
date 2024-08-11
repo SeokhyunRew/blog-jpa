9281371b84a32e36e341488bc2b7e06322de0395
SeokhyunRew <darkknightttt@naver.com>
2024-08-11 23:12:56 +0900

    feat: 입력 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -22,7 +22,9 @@ CREATE TABLE IF NOT EXISTS comment (
 INSERT INTO article (title, content, created_at, updated_at) VALUES ('제목1', '내용1', NOW(), NOW());
 INSERT INTO article (title, content, created_at, updated_at) VALUES ('제목2', '내용2', NOW(), NOW());
 INSERT INTO article (title, content, created_at, updated_at) VALUES ('제목3', '내용3', NOW(), NOW());
+INSERT INTO article (title, content, created_at, updated_at) VALUES ('제목4', '내용4', NOW(), NOW());
 
 INSERT INTO comment (article_id, body, created_at) VALUES (1, '내용1', NOW());
 INSERT INTO comment (article_id, body, created_at) VALUES (2, '내용2', NOW());
-INSERT INTO comment (article_id, body, created_at) VALUES (1, '내용3', NOW());
+INSERT INTO comment (article_id, body, created_at) VALUES (3, '내용3', NOW());
+INSERT INTO comment (article_id, body, created_at) VALUES (4, '내용4', NOW());
