e9e413f5474c48e705e8304b733f4a1c1ccb81ca
SeokhyunRew <darkknightttt@naver.com>
2024-08-12 22:48:32 +0900

    feat : log level도 일단 주석

--- src/main/java/com/example/blogjpa/aop/LogLevel.java
+++ src/main/java/com/example/blogjpa/aop/LogLevel.java
@@ -1,9 +1,13 @@
-package com.example.blogjpa.aop;/*
+/*
+package com.example.blogjpa.aop;*/
+/*
  * created by seokhyun on 2024-08-07.
- */
+ *//*
+
 
 public enum LogLevel {
 
     INFO,
     ERROR
 }
+*/
